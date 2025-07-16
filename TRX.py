73338664398d13a99cf0b85c0238679ffe9bbed6
JAKARIYA HASAN <140346514+SKBER-CYBER@users.noreply.github.com>
2025-07-17 02:57:11 +0600

    Update TRX.py

--- TRX.py
+++ TRX.py
@@ -1,8 +1,2 @@
-import platform,os
-#####
-os.system("git pull")
-bit = platform.architecture()[0]
-if bit == '64bit':
-    import trx1
-elif bit == '32bit':
-    print("SORRY BRO")
+import trx1
+trx1.run()
